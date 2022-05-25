a93e6ecbe68ee5d901ec2444eac61176164b5e75
mabril <maiicolabriil@hotmail.com>
2022-05-25 18:45:14 -0500

    Casos completos

--- cypress/integration/datos.spec.js
+++ cypress/integration/datos.spec.js
@@ -6,8 +6,10 @@ import {APPTIVIDAD} from "./caso5_apptividad";
 import { BIOMETRIA } from "./caso6-BiometriaKO";
 import { DIRECCION } from "./caso7-Direccion";
 import { TDD } from "./caso8-tdd";
-import { DECLARACIONJURADA } from "./caso9-declaracionJurada";*/
-import { BENEFICIARIOS } from "./caso10-beneficiarios";
+import { DECLARACIONJURADA } from "./caso9-declaracionJurada";
+import { BENEFICIARIOS } from "./caso10-beneficiarios";*/
+import {FLUJOSCOMPLETOS} from "./E2e-Cedulas"
+
 
 const loguin = {
   datosUsuario: {
@@ -30,7 +32,7 @@ const Tem_Productos = {
     emailmultipanama: "cmultipanama@yopmail.com",
     codemultipanama: "942786",
     emailmultitular: "cmultititular@yopmail.com",
-    codeMultititular: "767859",
+    codeMultititular: "767859"
   },
 };
 
@@ -164,62 +166,118 @@ const declaracionJurada = {
     salario: "3687",
     descripcionOtrosIngresos: "Ingresos varios",
     montoOtrosIngresos: "120",
-    montoApertura:"20000",
+    montoApertura: "20000",
     estadoCivilOp: "Casado(a)",
     estadoCivilValue: "MARRIED",
   },
 };
 
 const beneficiarios = {
-    datosUsuario: {
-      email: "beneficiarios@yopmail.com",
-      code: "264983",
-      nombres: "Michael Alejandro",
-      apellidos: "Abril Marmolejo",
-      fechaNacimiento: "13061994",
-      fechaexpedicion: "12122020",
-      fechaVencimiento: "12122030",
-      numeroRif: "V-23456756-2",
-    },
-  
-    direccion: {
-      urbanizacion: "SAN CRISTOBAL DE LA NUEVA GRAN",
-      calle: "CALLE 4K CON AVENIDAD PRINCIPAL 22S",
-      edificio: "EDIFICIO LOS ALTOS DE TORRE DE SOL ",
-      casa: "CASA 45 PI",
-      telefonoResidencial: "2136565654",
-      telefonoCelular: "4246556565",
-      puntoReferencia: "LA ESCUELA SAN MARTIN DE POTOS",
-    },
-    tdd: {},
-  
-    declaracion: {
-      situacionLaboralOp: "Asalariado",
-      situacionLaboralValue: "ASA",
-      tipoNegocioOp: "Empresas Financieras",
-      tipoNegocioValue: "3100",
-      ocupacionRiesgo: "Juez Fiscal",
-      ocupacionRiesgoValue: "1183",
-      ocupacionOp: "Banquero",
-      ocupacionValue: "1048",
-      salario: "3687",
-      descripcionOtrosIngresos: "Ingresos varios",
-      montoOtrosIngresos: "120",
-      montoApertura:"20000",
-      estadoCivilOp: "Casado(a)",
-      estadoCivilValue: "MARRIED",
-    },
+  datosUsuario: {
+    email: "beneficiarios@yopmail.com",
+    code: "264983",
+    nombres: "Michael Alejandro",
+    apellidos: "Abril Marmolejo",
+    fechaNacimiento: "13061994",
+    fechaexpedicion: "12122020",
+    fechaVencimiento: "12122030",
+    numeroRif: "V-23456756-2",
+  },
 
-    beneficiariosData: {
-        Bnombres: "Michael Alejandro    ",
-        Bapellidos: "Abril Marmolejo",
-        BfechaNacimiento: "1994-06-06",
-        Bporcentaje: "100",
-        Bidentificacion: "CC11116263511",
-        Btelefono: "3154258963",
-        Bemail: "mabril@test.com"
-      }
-  };
+  direccion: {
+    urbanizacion: "SAN CRISTOBAL DE LA NUEVA GRAN",
+    calle: "CALLE 4K CON AVENIDAD PRINCIPAL 22S",
+    edificio: "EDIFICIO LOS ALTOS DE TORRE DE SOL ",
+    casa: "CASA 45 PI",
+    telefonoResidencial: "2136565654",
+    telefonoCelular: "4246556565",
+    puntoReferencia: "LA ESCUELA SAN MARTIN DE POTOS",
+  },
+  tdd: {},
+
+  declaracion: {
+    situacionLaboralOp: "Asalariado",
+    situacionLaboralValue: "ASA",
+    tipoNegocioOp: "Empresas Financieras",
+    tipoNegocioValue: "3100",
+    ocupacionRiesgo: "Juez Fiscal",
+    ocupacionRiesgoValue: "1183",
+    ocupacionOp: "Banquero",
+    ocupacionValue: "1048",
+    salario: "3687",
+    descripcionOtrosIngresos: "Ingresos varios",
+    montoOtrosIngresos: "120",
+    montoApertura: "20000",
+    estadoCivilOp: "Casado(a)",
+    estadoCivilValue: "MARRIED",
+  },
+
+  beneficiariosData: {
+    Bnombres: "Michael Alejandro    ",
+    Bapellidos: "Abril Marmolejo",
+    BfechaNacimiento: "1994-06-06",
+    Bporcentaje: "100",
+    Bidentificacion: "CC11116263511",
+    Btelefono: "3154258963",
+    Bemail: "mabril@test.com",
+  },
+};
+
+const e2e = {
+  datosUsuario: {
+    emailCtaAhorro: "ctaAhorro@yopmail.com", codeCtaAhorro: "586895",
+    emailCtaEuro:"ctaEuros@yopmail.com", codeCtaEuro: "656984",
+    emailCtaCorriente: "ctaCorriente@yopmail.com", codeCtaCorriente: "396963",
+    emailCtaMultipanama:"ctaMultipanama@yopmail.com", codeCtaMultipanama: "347398",
+    emailCtaMultititular00: "ctaMultititular00@yopmail.com", codeCtaMultititular00: "292599",
+    emailCtaMultititular01: "ctaMultititular01@yopmail.com", codeCtaMultititular01: "889536",
+    emailCtaSimplificada:"ctaSimplificada@yopmail.com", codeCtasimplificada: "276783",
+    nombres: "Michael Alejandro",
+    apellidos: "Abril Marmolejo",
+    fechaNacimiento: "13061994",
+    fechaexpedicion: "12122020",
+    fechaVencimiento: "12122030",
+    numeroRif: "V-23456756-2",
+  },
+
+  direccion: {
+    urbanizacion: "SAN CRISTOBAL DE LA NUEVA GRAN",
+    calle: "CALLE 4K CON AVENIDAD PRINCIPAL 22S",
+    edificio: "EDIFICIO LOS ALTOS DE TORRE DE SOL ",
+    casa: "CASA 45 PI",
+    telefonoResidencial: "2136565654",
+    telefonoCelular: "4246556565",
+    puntoReferencia: "LA ESCUELA SAN MARTIN DE POTOS",
+  },
+  tdd: {},
+
+  declaracion: {
+    situacionLaboralOp: "Asalariado",
+    situacionLaboralValue: "ASA",
+    tipoNegocioOp: "Empresas Financieras",
+    tipoNegocioValue: "3100",
+    ocupacionRiesgo: "Juez Fiscal",
+    ocupacionRiesgoValue: "1183",
+    ocupacionOp: "Banquero",
+    ocupacionValue: "1048",
+    salario: "3687",
+    descripcionOtrosIngresos: "Ingresos varios",
+    montoOtrosIngresos: "120",
+    montoApertura: "20000",
+    estadoCivilOp: "Casado(a)",
+    estadoCivilValue: "MARRIED",
+  },
+
+  beneficiariosData: {
+    Bnombres: "Michael Alejandro    ",
+    Bapellidos: "Abril Marmolejo",
+    BfechaNacimiento: "1994-06-06",
+    Bporcentaje: "100",
+    Bidentificacion: "CC11116263511",
+    Btelefono: "3154258963",
+    Bemail: "mabril@test.com",
+  },
+};
 var planilla = [declaracionJurada, casepep];
 /*LOGUIN(loguin)
 PROD_TEMPLATES(Tem_Productos)
@@ -229,6 +287,7 @@ APPTIVIDAD(apptividad)
 BIOMETRIA(biometria)
 DIRECCION(direccion);
 TDD(tdd);
-DECLARACIONJURADA(declaracionJurada);*/
-BENEFICIARIOS(beneficiarios)
+DECLARACIONJURADA(declaracionJurada);
+BENEFICIARIOS(beneficiarios);*/
+FLUJOSCOMPLETOS (e2e)
 //PLANILLA(planilla)
